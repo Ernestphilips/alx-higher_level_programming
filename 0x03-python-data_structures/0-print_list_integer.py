412817a2ee49c70405cb66da145a74f08088cfc9
Ernest <ernest1philips@gmail.com>
2022-09-26 22:44:56 +0000

    task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,20 +1,5 @@
-#include <Python.h>
-#include <object.h>
-#include <listobject.h>
+#!/usr/bin/python3
 
-/**
-* print_python_list_info - prints basic info about pytohn list
-* @p: python object
-**/
-
-void print_python_list_info(PyObject *p)
-{
-	long int size = PyList_Size(p);
-	int i;
-	PyListObject *obj = (PyListObject *)p;
-
-	printf("[*] Size of the Python List = %li\n", size);
-	printf("[*] Allocated = %li\n", obj->allocated);
-	for (i = 0; i < size; i++)
-		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
-}
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print('{:d}'.format(i))
